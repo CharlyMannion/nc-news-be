42a085935871b8d1b51e87e8ed79c68d7b5458a6
JFParrott <jfparrott@protonmail.com>
2020-10-16 12:52:30 +0100

    add heroku hosting config

--- db/connection.js
+++ db/connection.js
@@ -1,4 +1,9 @@
-const config = require('../knexfile')
-const connection = require('knex')(config);
+const ENV = process.env.NODE_ENV || 'development';
+const connection = require('knex');
 
-module.exports = connection;
+const dbConfig =
+  ENV === 'production'
+    ? { client: 'pg', connection: process.env.DATABASE_URL }
+    : require('../knexfile');
+
+module.exports = connection(dbConfig);
